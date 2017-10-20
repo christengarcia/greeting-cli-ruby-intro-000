f1dbc6d70924e3ec4bedf221b9d4e16cd71c77b2
Christen Garcia <christen.garcia@gmail.com>
2017-10-20 00:19:04 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,7 @@
 def greeting(name)
-  puts "Hello #{name}. It's nice to meet you."
+  puts "What is your name?"
   name = gets.strip
-end
+  puts "Hello #{name}. It's nice to meet you."
+=end
 
 greeting
